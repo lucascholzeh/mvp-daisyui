2a2357ea8260f9cf1e83f337e6f64ceca681d5bf
Lucas Hoffmann <lucasschoff@gmail.com>
2026-03-24 16:59:10 -0300

    basic layout page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="navbar bg-neutral text-neutral-content">
+    <button class="btn btn-ghost text-xl">MVP Rick and Morty</button>
+  </div>
+</template>
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import axios from 'axios'
+
+const episodes = ref([])
+
+const text = ref()
+
+async function handleGetEpisodes() {
+  try {
+    const response = await axios.get('https://rickandmortyapi.com/api/episode')
+    episodes.value = response.data.results
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+onMounted(() => {
+  handleGetEpisodes()
+})
+</script>
+
+<template>
+  <div class="p-4">
+    <pre>
+        {{ episodes }}
+    </pre>
+  </div>
+</template>
