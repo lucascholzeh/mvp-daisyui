b2169b6d350ff380a82a864ca284a2be409bee63
Lucas Hoffmann <lucasschoff@gmail.com>
2026-03-24 16:57:09 -0300

    add auto import

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+
+        /\.vue$/, // .vue
+
+        /\.vue\?vue/, // .vue?vue
+
+        /\.md$/, // .md
+      ],
+      imports: ['vue'],
+    }),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue'],
+      deep: true,
+      dts: false,
+      include: [/\.vue$/, /\.vue\?vue/, /\.vue\.[tj]sx?\?vue/],
+      exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
+      excludeNames: [/^Async.+/],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
