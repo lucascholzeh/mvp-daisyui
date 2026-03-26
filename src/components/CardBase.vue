45980a86c65b906c1eef8277a2675936e357f0b2
Lucas Hoffmann <lucasschoff@gmail.com>
2026-03-26 17:38:10 -0300

    add episode cards

---
+++ src/components/CardBase.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="card bg-base-100 shadow-sm hover:shadow-md hover:bg-neutral-content cursor-pointer">
+    <div class="avatar-group -space-x-6">
+      <div class="avatar" v-for="character in charactersFilter">
+        <div class="w-12">
+          <img :src="getImage(character)" />
+        </div>
+      </div>
+      <div v-if="charactersCount" class="avatar avatar-placeholder">
+        <div class="bg-neutral text-neutral-content w-12">
+          <span>+{{ charactersCount }}</span>
+        </div>
+      </div>
+    </div>
+    <div class="card-body">
+      <h2 class="card-title">
+        {{ name }}
+        <div class="badge badge-secondary">{{ episode }}</div>
+      </h2>
+      <div class="card-actions justify-end">
+        <div class="badge badge-outline">{{ air_date }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: String,
+  episode: String,
+  air_date: String,
+  characters: Array,
+})
+
+const charactersFilter = computed(() => {
+  if (props.characters.length > 5) {
+    return props.characters.splice(0, 5)
+  }
+  return props.characters
+})
+
+const charactersCount = computed(() => {
+  if (props.characters.length > 5) {
+    const count = props.characters.length - 5
+    return count
+  }
+  return 0
+})
+
+function getImage(url) {
+  const newUrl = url.replace(
+    'https://rickandmortyapi.com/api/character/',
+    'https://rickandmortyapi.com/api/character/avatar/',
+  )
+  return `${newUrl}.jpeg`
+}
+</script>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CardBase from '@/components/CardBase.vue'
 import axios from 'axios'
 
 const episodes = ref([])
@@ -21,6 +22,16 @@ onMounted(() => {
 
 <template>
   <div class="p-4">
+    <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <CardBase
+        v-for="episode in episodes"
+        :key="episode.id"
+        :name="episode.name"
+        :episode="episode.episode"
+        :air_date="episode.air_date"
+        :characters="episode.characters"
+      />
+    </div>
     <pre>
         {{ episodes }}
     </pre>
